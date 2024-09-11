6805e8e36bcd74cc639c550d48805442691b92d8
VanChanh <vanchanh0730@gmai.com>
2024-09-11 16:32:11 +0700

    change structure of project, complete 2 screen

--- src/navigation/NavigationStack.js
+++ src/navigation/NavigationStack.js
@@ -4,14 +4,17 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 
-import SignIn from "../view/SignIn";
-import SignUp from "../view/SignUp";
-import Verification from "../view/Verification";
-import RequestResetPassword from "../view/RequestResetPassword";
-import Account from "../view/Account";
-import ListVocabulary from "../view/ListVocabulary";
-import Home from "../view/Home";
-import ResetPassword from "../view/ResetPassword";
+import SignIn from "../view/Authen/SignIn";
+import SignUp from "../view/Authen/SignUp";
+import Verification from "../view/Authen/Verification";
+import RequestResetPassword from "../view/Authen/RequestResetPassword";
+import Account from "../view/TabScreen/Account";
+import ListVocabulary from "../view/TabScreen/ListVocabulary";
+import Home from "../view/TabScreen/Home";
+import ResetPassword from "../view/Authen/ResetPassword";
+import TruyenChem_S1 from "../view/truyenChem/TruyenChem_S1";
+import TruyenChem_S2 from "../view/truyenChem/TruyenChem_S2";
+import TruyenChem_S3 from "../view/truyenChem/TruyenChem_S3";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -66,40 +69,65 @@ const NavigationStack = () => {
           name="SignIn"
           component={SignIn}
           options={{ headerShown: false }}
-        />
-        <Stack.Screen
+        /> */}
+
+        {/* <Stack.Screen
           name="SignUp"
           component={SignUp}
           options={{ headerShown: false }}
-        />
-        <Stack.Screen
+        /> */}
+
+        {/* <Stack.Screen
           name="Verification"
           component={Verification}
           options={{ headerShown: false }}
-        />
-        <Stack.Screen
+        /> */}
+
+        {/* <Stack.Screen
           name="RequestResetPassword"
           component={RequestResetPassword}
           options={{ headerShown: false }}
-        />
-        <Stack.Screen
+        /> */}
+
+        {/* <Stack.Screen
           name="ResetPassword"
           component={ResetPassword}
           options={{ headerShown: false }}
-        />*/}
-        <Stack.Screen
+        /> */}
+
+        {/* <Stack.Screen
           name="TabNavigationContainer"
           component={TabNavigationContainer}
           options={{ headerShown: false }}
-        />
+        /> */}
+
         {/* <Stack.Screen
           name="ListVocabulary"
           component={ListVocabulary}
           options={{ headerShown: false }}
         /> */}
+
         {/* <Stack.Screen
           name="Account"
           component={Account}
+          options={{ headerShown: false }}
+        /> */}
+
+        <Stack.Screen
+          name="TruyenChem_S1"
+          component={TruyenChem_S1}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="TruyenChem_S2"
+          component={TruyenChem_S2}
+          options={{ headerShown: false }}
+        />
+
+        {/* <Stack.Screen
+          name="TruyenChem_S3"
+          component={TruyenChem_S3}
           options={{ headerShown: false }}
         /> */}
       </Stack.Navigator>
